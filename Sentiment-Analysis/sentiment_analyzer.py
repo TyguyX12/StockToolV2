15143323a3ab5694c5342059a903bb73b4ba699f
Tyler Marinkovic <50028138+TyguyX12@users.noreply.github.com>
2021-11-23 13:26:45 -0600

    Update sentiment_analyzer.py
    Removed weekly & hourly sentiment analysis

--- Sentiment-Analysis/sentiment_analyzer.py
+++ Sentiment-Analysis/sentiment_analyzer.py
@@ -4,14 +4,6 @@ import time
 import datetime
 import _thread
 
-
-def weekly_sentiment(twitter):
-    while 1:
-        twitter.run_from_db('weekly')
-        time.sleep(604800)
-
-
-
 if __name__ == '__main__':
     last_day = ''
     tw = TwitterSentiment()
@@ -21,10 +13,5 @@ if __name__ == '__main__':
         if day != last_day:
             last_day = day
             #function call
-            tw.run_from_db('daily')
+            tw.run_from_db()
         
-
-        tw.run_from_db('hourly')
-        rd.run_reddit_sentiment()
-        _thread.start_new_thread(weekly_sentiment, (tw, ))
-        time.sleep(3600)
