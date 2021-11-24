7f6c9d2b73152ee8e944b5a9226e56deb48262a1
Tyler Marinkovic <50028138+TyguyX12@users.noreply.github.com>
2021-11-23 18:17:03 -0600

    Update twitter_stock_sentiment.py
    Provided a lot of documentation and modified variables for clarity.

--- Sentiment-Analysis/sentiment_analysis/twitter_stock_sentiment.py
+++ Sentiment-Analysis/sentiment_analysis/twitter_stock_sentiment.py
@@ -24,152 +24,162 @@ class TwitterSentiment(object):
             self.vader = SentimentIntensityAnalyzer()
         self.vader.lexicon.update(new_words)
 
+    #   RUN_FROM_DB:
+    #   Calls method to pull tweets from the db for a given date
+    def run_from_db(self, date):                                                    #   REMOVED TYPE, ADDED DATE
+        SortedCounts, TweetTexts = self.process_tweets_from_db(date)                #   ADDED DATE. SortedCounts = list of stocks sorted by highest count
+                                                                                    #   SortedCounts is a dict with a stock name as it's keys and the no. of references as a value
+                                                                                    #   TweetTexts is a dict with a stock name as it's keys and TweetTexts as values
+        #print(self.process_tweets_from_db())
+        scores = self.analyze_sentiment(SortedCounts, TweetTexts)                   #   Gather sentiment scores for all tweets & aggregate as scores
+        
+        insert_sentiment_scores(CONNECTION, scores, 'twitter', date)                #   REMOVED TYPE, ADDED DATE. 
+        #self.display_results(scores)
+
     
-    def process_tweets_from_db(self, sentiment_type):
-        tweets = gather_tweets_from_db(CONNECTION, sentiment_type)
-        tickers, comments = {}, {}
+    #   Calls tweets from the db for a given date
+    def process_tweets_from_db(self, date):                                         #   ADDED DATE, Tweets will be pulled for a given date for sentiment analysis
+        tweets = gather_tweets_from_db(CONNECTION, date)                            #   ADDED DATE
+        TweetCount, TweetTexts = {}, {}                                             #   RENAMED tickers & comments TO TweetCount & TweetTexts FOR CLARITY.
+                                                                                    #   TweetCount is a dict with a stock name as it's keys and the no. of references as a value
+                                                                                    #   TweetTexts is a dict with a stock name as it's keys and TweetTexts as values. Returned by this function
         fetch = tweets.fetchmany
         tweet_ids = []
         while True:
-            rows = fetch(2000)
-            if not rows: break
+            tweetsFromDB = fetch(2000)                                              #   RENAMED rows to tweetsFromDB
+            if not tweetsFromDB: break
             
-            for row in rows:
-                #print(row)
+            for tweet in tweetsFromDB:                                              #   tweetsFromDB = 2000 Tweets fetched from DB
+                #print(tweet)
                 try:
-                    #print(row)
-                    words = row['TweetText'].split(" ")
+                    #print(tweet)
+                    words = tweet['TweetText'].split(" ")                           #   Words = words in the Tweet
 
                     for word in words:
                         word = word.replace("\\n", "")
                         word = word.replace("$", "")
 
-                        if word.isupper() and word in us and word not in blacklist:
+                        #   Determines whether or not the Tweet is discussing a given stock 
+                        #   This will likely be changed due to the new Tweet collection method including which stock is discussed
+                        if word.isupper() and word in us and word not in blacklist:     
 
-                            if word.upper() not in tickers:
-                                tickers[word.upper()] = 1
-                                comments[word.upper()] = [row['TweetText']]
-                            else:
-                                tickers[word.upper()] += 1
-                                comments[word.upper()].append(row['TweetText'])
+                            if word.upper() not in TweetCount:                      #   If a stock has not yet had a Tweet about it, create an index in TweetCount and TweetTexts 
+                                TweetCount[word.upper()] = 1                    
+                                TweetTexts[word.upper()] = [row['TweetText']]       
+                            else:                                                   #   Otherwise, TweetCount is incremented, and TweetTexts gets a new row with another Tweet's text
+                                TweetCount[word.upper()] += 1
+                                TweetTexts[word.upper()].append(row['TweetText'])
                 
                 except Exception as e:
                     print(e)
                 
-                tweet_ids.append(row['idTwitterData'])
+                tweet_ids.append(row['idTwitterData'])                              #   TweetIds are stored so that they may be marked as processed
         
         tweets.close()
-        update_processed_col(CONNECTION, tweet_ids, sentiment_type)
+        update_processed_col(CONNECTION, tweet_ids)                                 #   REMOVED TYPE. Marks Tweets for deletion after they are processed
             
-
-        symbols = dict(sorted(tickers.items(), key=lambda item: item[1], reverse=True))
-        self.top_picks = list(symbols.keys())[0:self.__top_picks]
+            
+        SortedCounts = dict(sorted(TweetCount.items(), key=lambda item: item[1], reverse=True))  #   RENAMED SYMBOLS TO SortedCounts. Sorts TweetCounts with most Tweets First, returned by this function
+        self.top_picks = list(SortedCounts.keys())[0:self.__top_picks]                           #   Contains top_picks no. of Stocks with the most Tweets about them. May be reevaluated to include all Tweets about top Reddit stocks.
 
         config.LOGGER.info("Finished processing tweets from the DB.")
-        return (symbols, comments)
-
-
-    def process_tweets_from_excel(self):
-        file_tags = ['stocks']
-        data_dir = os.getcwd() + os.path.sep + 'data' + os.path.sep
-
-        tickers, comments = {}, {}
-
-        for tag in file_tags:
-            tweet_dict = pd.read_excel(data_dir + '{}_hashtag.xlsx'.format(tag)).to_dict()
-            
-            for tweet in tweet_dict['text']:
-                #logic here for sentiment analysiss
-                print(tweet_dict['text'][tweet])
-                words = str(tweet_dict['text'][tweet]).split(" ")
-
-                for word in words:
-                    word = word.replace("\\n", "")
-                    word = word.replace("$", "")
-
-                    if word.isupper() and word in us and word not in blacklist:
-                        
-                        if word.upper() not in tickers:
-                            tickers[word.upper()] = 1
-                            comments[word.upper()] = [tweet_dict['text'][tweet]]
-                        else:
-                            tickers[word.upper()] += 1
-                            comments[word.upper()].append(tweet_dict['text'][tweet])
-
-        symbols = dict(sorted(tickers.items(), key=lambda item: item[1], reverse=True))
-        self.top_picks = list(symbols.keys())[0:self.__top_picks]
-        return symbols, comments
+        return (SortedCounts, TweetTexts)                                           #   Returns the sorted list of most mentioned stocks, as well as the Tweets about them, to be used in the following sentiment analysis
     
 
-    def analyze_sentiment(self, symbols, comments):
-
+    
+    #   This function takes in the list of stocks sorted by TweetCount, as well as the actual Tweet texts themselves
+    def analyze_sentiment(self, SortedCounts, TweetTexts):
         print(f"\n{self.__top_picks} most mentioned picks: ")
-        self.times = []
-        self.top = []
-        for i in self.top_picks:
-            print(f"{i}: {symbols[i]}")
-            self.times.append(symbols[i])
-            self.top.append(f"{i}: {symbols[i]}")
+#        self.times = []                                                            #   COMMENTED OUT TIMES, ONLY USED IN GRAPHING. Contains list of times that each stock is mentioned (detached from name). 
+#        self.top = []                                                              #   COMMENTED OUT TOP, ONLY USED IN GRAPHING. Contains String-formatted list "Stock: Count" with all top picks
+        for stock in self.top_picks:                                                #   Runs through the top_picks (30 most mentioned stocks) and does the following for each stock
+            print(f"{stock}: {SortedCounts[stock]}")
+#            self.times.append(SortedCounts[stock])                                 #   COMMMENTED OUT TIMES CODE, ONLY USED IN GRAPHING
+#            self.top.append(f"{stock}: {SortedCounts[stock]}")                     #   COMMMENTED OUT TOP CODE, ONLY USED IN GRAPHING
    
         print("Beginning sentiment analysis...")    
-
-        scores, s = {}, {}
-
-
-        picks_sentiment = list(symbols.keys())[0:self.__top_picks]
-        for symbol in picks_sentiment:
-            stock_comments = comments[symbol]
-            for comment in stock_comments:
-                score = self.vader.polarity_scores(comment)
-                if symbol in s:
-                    s[symbol][comment] = score
-                else:
-                    s[symbol] = {comment:score}
-                if symbol in scores:
+        
+        tweetscores, cumulativescores = {}, {}                                      #   RENAMED s TO tweetscores, scores TO cumulativescores 
+                                                                                    #   tweetscores contains all neg, neu, and pos sentiment for all of a stock's tweets.
+                                                                                    #   cumulativescores contains an average of all neg, neu, and pos sentiment for a stock.  
+                                                                                    
+        
+        picks_sentiment = list(SortedCounts.keys())[0:self.__top_picks]             #   SEEMS REDUNDANT, SAME AS self.top_picks?
+        for stock in picks_sentiment:                                               #   RENAMED SYMBOL TO STOCK
+            tweetsForStock = TweetTexts[stock]                                      #   RENAMED stock_comments TO tweetsForStock FOR CLARITY
+            for tweet in tweetsForStock:                                            #   RENAMED comment TO tweet FOR CLARITY
+                tweetscore = self.vader.polarity_scores(tweet)                      #   RENAMED score to tweetScore FOR CLARITY. tweetScore = neg, neu, pos, and compound scores for a given stock
+                
+                if stock in tweetscores:                                            #   Adds score to tweetscores at index tweet if a Tweet has been processed for it
+                    tweetscores[stock][tweet] = tweetscore                                       
+                else:                                                               #   Initializes tweetscores if a Tweet has not yet been processed for it.
+                    tweetscores[stock] = {tweet:tweetscore}
+                
+                if stock in cumulativescores:                                       #   Adds score to cumulativescores at if a Tweet has been processed for it
                     for key, _ in score.items():
-                        scores[symbol][key] += score[key]
-                else:
-                    scores[symbol] = score
+                        cumulativescores[stock][key] += score[key]
+                else:                                                               #   Initializes cumulativescores if a Tweet has not yet been processed for it.
+                    cumulativescores[stock] = score
 
-            for key in score:
-                scores[symbol][key] = scores[symbol][key] / symbols[symbol]
-                scores[symbol][key] = "{pol:.3f}".format(pol=scores[symbol][key])
+            for sentimentType in tweetScore:                                        #   RENAMED key TO sentiment_type (neg, neu, pos) FOR CLARITY. Runs through all scores for a stock 
+                cumulativescores[stock][sentimentType] = cumulativescores[stock][sentimentType] / SortedCounts[stock]           #   The cumulative score for each type of sentiment (neg, neu, pos) is an average of all scores of that type. 
+                cumulativescores[stock][sentimentType] = "{pol:.3f}".format(pol=cumulativescores[stock][sentimentType])         #   Formats cumulativescores to 3 decimals
 
-        return scores
-
-    
-    def display_results(self, scores):
-        df = pd.DataFrame(scores)
-        df.index = ['Bearish', 'Neutral', 'Bullish', 'Total/Compound']
-        df = df.T
-
-        squarify.plot(sizes=self.times, label=self.top, alpha=.7)
-        plt.axis('off')
-        plt.title(f"{self.__top_picks} most mentioned picks")
-        plt.show()  
-
-        #Sentiment Analysis
-        df = df.astype(float)
-        colors = ['red', 'springgreen', 'forestgreen', 'coral']
-        df.plot(kind = 'bar', color=colors, title=f"Sentiment analysis of top {self.__top_picks} picks:")
-        plt.show()
-
-
-    def run_from_excel(self):
-        symbols, comments = self.process_tweets_from_excel()
-        scores = self.analyze_sentiment(symbols, comments)
-        self.display_results(scores)
+        return cumulativescores
 
     
-    def run_from_db(self, sentiment_type):
-        symbols, comments = self.process_tweets_from_db(sentiment_type)
-        #print(self.process_tweets_from_db())
-        scores = self.analyze_sentiment(symbols, comments)
-        insert_sentiment_scores(CONNECTION, scores, 'twitter', sentiment_type)
-        #self.display_results(scores)
+#    def display_results(self, scores):
+#        df = pd.DataFrame(scores)
+#        df.index = ['Bearish', 'Neutral', 'Bullish', 'Total/Compound']
+#        df = df.T
+#
+#        squarify.plot(sizes=self.times, label=self.top, alpha=.7)
+#        plt.axis('off')
+#        plt.title(f"{self.__top_picks} most mentioned picks")
+#        plt.show()  
+#
+#        #Sentiment Analysis
+#        df = df.astype(float)
+#        colors = ['red', 'springgreen', 'forestgreen', 'coral']
+#        df.plot(kind = 'bar', color=colors, title=f"Sentiment analysis of top {self.__top_picks} picks:")
+#        plt.show()   
 
-        #FIND A WAY TO INSERT_PROCESSED_SCORE WITH ASSET NAME & SCORE
-        #insert_processed_sentiment_score(CONNECTION, asset, score, 'twitter')
+
+#    def process_tweets_from_excel(self):
+#        file_tags = ['stocks']
+#        data_dir = os.getcwd() + os.path.sep + 'data' + os.path.sep
+#
+#        TweetCount, TweetTexts = {}, {}
+#
+#        for tag in file_tags:
+#            tweet_dict = pd.read_excel(data_dir + '{}_hashtag.xlsx'.format(tag)).to_dict()
+#            
+#            for tweet in tweet_dict['text']:
+#                #logic here for sentiment analysiss
+#                print(tweet_dict['text'][tweet])
+#                words = str(tweet_dict['text'][tweet]).split(" ")
+#
+#                for word in words:
+#                    word = word.replace("\\n", "")
+#                    word = word.replace("$", "")
+#
+#                    if word.isupper() and word in us and word not in blacklist:
+#                        
+#                        if word.upper() not in TweetCount:
+#                            TweetCount[word.upper()] = 1
+#                            TweetTexts[word.upper()] = [tweet_dict['text'][tweet]]
+#                        else:
+#                            TweetCount[word.upper()] += 1
+#                            TweetTexts[word.upper()].append(tweet_dict['text'][tweet])
+#
+#        symbols = dict(sorted(TweetCount.items(), key=lambda item: item[1], reverse=True))
+#        self.top_picks = list(symbols.keys())[0:self.__top_picks]
+#        return symbols, TweetTexts        
+
+#    def run_from_excel(self):
+#        symbols, TweetTexts = self.process_tweets_from_excel()
+#        scores = self.analyze_sentiment(symbols, TweetTexts)
+#        self.display_results(scores)
 
 ###UNIT TESTS
 if __name__ == '__main__':
