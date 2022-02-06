6c40c7388ba7ca6f13ad2cd0b45e24e1ce454d44
Tyler Marinkovic <50028138+TyguyX12@users.noreply.github.com>
2022-02-06 13:18:20 -0600

    Delete deploy.sh

--- Twitter-Listener/deploy.sh
+++
@@ -1,6 +0,0 @@
-pkill -f -9 /home/stocktool/twitter-api-listener/__main__.py
-source /home/stocktool/envs/ci.env
-source /home/stocktool/env/bin/activate
-cd /home/stocktool/twitter-api-listener
-git pull
-(nohup python /home/stocktool/twitter-api-listener/__main__.py &) && sleep 1
