da756296e655c380fa1365b834d7f31e792f286d
Tyler Marinkovic <50028138+TyguyX12@users.noreply.github.com>
2022-02-06 13:00:29 -0600

    Update sentiment_analyzer.py
    Moved CreateSUPERCSV to this file and edited plans for the future.

--- Sentiment-Analysis/sentiment_analyzer.py
+++ Sentiment-Analysis/sentiment_analyzer.py
@@ -3,30 +3,70 @@ from reddit_stock_sentiment import RedditSentiment
 import time
 import datetime
 import _thread
+import csv
+from config import *
 
 tw = TwitterSentiment()
 rd = RedditSentiment()
 
-DATES = [datetime.date(YYYY, MM, D)]
+#DATES = [datetime.date(2021, 12, 7)]
+TODAY = datetime.date.today()
 
 #dates[] = [date1, date2, date3]
 
 
-if __name__ == '__main__':
-    for date in DATES:
-        print("Running for: " + str(date))
-        #rd.run_for_date(date)
-        tw.run_for_date(date)                      
+def createSuperCSV(threshold):
+    #   Creates the superCSV file that determines stocks that should be read every day, parameterized by threshold
+    #   If a stock appears more than threshold amount of times over the course of all of the csv files, it is added to the superCSV
+    #   The schema of the superCSV is as follows: [stockName,   requiresAdditionalHashtag   tweetWeight] 
+    #   Where requiresAdditionalHashtag is a boolean indicating whether or not the stock requires a second stock-related hashtag (ex. a 1-2 character stock, or a common word)  
+    #   and tweetWeight is the number of Tweets that are collected in one scrape of the Tweet Collector (NOT IMPLEMENTED)
+    #   This algorithm may be imperfect, and may require additional dilligence on behalf of the user to make sure common words are not included ("Ex. HOPE")
 
-                    
+        StockCounts, StockAdditionalHashtags = {}, {}                                              #   StockCounts keeps track of stock names and counts while iterating over all of the stocks
+                                                                                                   #   StockAdditionalHashtags keeps track of stock names and whether or not they will need an additional hashtag.
+
+        superCSVPath = open(('/users/tymar/downloads/Schoolwork/Capstone/Twitter/superCSV.csv'), 'w')
+        superCSVWriter = csv.writer(superCSVPath)
+
+        scorePath = '/users/tymar/downloads/Schoolwork/Capstone/Reddit/RedditScores/'
+        for fileName in os.listdir(scorePath):
+            filePath = str(scorePath) + str(fileName)
+            with open(filePath, newline='\n') as csvfile:
+                csvReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+                for line in csvReader:
+                    #   Skips top line of each CSV
+                    if str(line[0]) ==  'source':
+                        continue
+                    #   Handles adding instances of stocks to StockCounts
+                    if line[2] in StockCounts:                                                      #   An instance has already been processed for this stock
+                        StockCounts[line[2]] += int(line[7])
+                    else: 
+                        StockCounts[line[2]] = int(line[7])
+        
+        #Orders stocks by StockCount and adds them to superCSV.csv
+        superCSVWriter.writerow(["stock", "redditInstances"])
+        for stock in reversed(sorted(StockCounts.items(), key=lambda item: item[1])):
+            if stock[1] >= threshold:
+                superCSVWriter.writerow([str(stock[0]), str(StockCounts[stock[0]])])
+
+        superCSVPath.close()
+    
+if __name__ == '__main__':
+    createSuperCSV(3)
+    tw.run_for_date(TODAY)                
+    rd.run_for_date(TODAY)
 
             
-#       This code is likely going to be heavily modified (or replaced). The current plan includes collecting historical
-#   tweets and reddit posts using their respective APIs. The Reddit data is collected first, and among the most mentioned
-#   reddit stocks, Tweets are collected. This will be run for each day, backwards, for as long as we are able to collect data.
-#   This sentiment_analyzer will be a script in which a day is given as a parameter, and sentiment analysis is done for the 
-#   Twitter and Reddit posts for that day. This score is saved in the sentimentdata table and includes:
+#       This code gathers sentiment scores for both Twitter and Reddit for a given day, as well as creating the SuperCSV
+#       The Twitter Process is quicker, as it must simply run through all of the tweets collected for that day
+#       The Reddit Process takes longer, as it must search through all of the top Reddit posts for that day, collect them, and then run sentiment analysis
+#       Both of these must be run every day, missing a day with the Twitter Process is okay, because the Tweets are stored
+#       If the Reddit Process is missed, a majority of the posts for that day will be unavailable
+#       As of now, This may be implemented as a second Windows Service, timed to run at 9PM every night
 
+#       SCHEMA FOR SentimentScore CSV
+#       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #       `id`                (PRIMARY KEY)
 #       `source`            (TWITTER OR REDDIT)
 #       `asset`             (STOCK NAME)
